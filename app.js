4df4f534d7289187bf2f4aab46681c5308d810ce
basilkhan1250 <basilkhan1250@gmail.com>
2024-02-22 07:28:09 -0800

    JS practice

---
+++ app.js
@@ -0,0 +1,232 @@
+// quesion  1
+
+// var value = prompt ("Enter your value")
+// var remainder = value % 3 
+// if (remainder === 0){
+// document.write("This is divisible with " + 3 )
+// }
+// else {
+// document.write("This is not divisible with " + 3  )
+// }
+
+// question 2 
+
+// var a = prompt ("Enter your number")
+// var b = a % 2 
+// if (b === 0){
+//     document.write ("This number is even " + a)
+// }
+// else {
+//     document.write ("This number is odd " + a)
+// }
+
+// question  3
+
+// var age = +prompt("enter your age")
+// if (age > 18){
+//     console.log("old enough")
+// }
+// else {
+//     console.log("too young")
+// }
+
+// question 4
+
+
+// var myName = "basil"
+// var askName = prompt("enter your name")
+// if (myName === askName){
+//     console.log("Our names are same, What a coincidence, Well have a NICE DAY")
+// }
+// else {
+//     console.log(" ")
+// }
+
+// question 6
+
+
+
+// question 7
+
+// var a = +prompt("first number ")
+// var b = prompt("operator \n 1: + \n 2: - \n 3: /  \n 4: * \n 5: % " )
+// var c = +prompt("second number")
+//  if (b === "1" ){
+//     document.write ( a + c)
+// }
+// else if (b === "2"){
+//     document.write (a - c)
+// }
+// else if (b === "3"){
+//     document.write (a / c)
+// } 
+// else if (b === "4"){
+//     document.write (a * c)
+// }
+// else if (b === "5"){
+//     document.write ( a % c)
+// }
+// else {
+//     document.write ("syntex error")
+// }
+
+
+
+
+// question 10
+
+// var a = prompt ("input your password")
+// if (a === "BASILKHAN"){
+//     document.write ("this is the correct password ")
+// }
+
+// else if (a){
+//     document.write("this is wrong password")
+// }
+// else {
+//     document.write ("please enter your password")
+// }
+
+
+// question 11
+
+// var firstName = "Ali";
+// if (firstName === "Fahad") {
+//  document.write("Hello Fahad!");
+// }
+// else {
+//     document.write("You are not Fahad")
+// }
+
+
+// question 12
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+//     console.log(greeting = "Good day");
+// }
+// else{
+//     console.log(greeting = "Good evening");
+// }
+
+// question 13
+
+// var value1 = +prompt("enter your 1st value")
+// var value2 = +prompt("enter your 2nd value")
+// if (value1 > value2){
+//     console.log("this is the first value and it's larger "+value1)
+// }
+// else if (value1 < value2){
+//     console.log("this is the second value and it's larger "+value2)
+// }
+// else if (value1 === value2){
+//     console.log(value1 + " = " + value2 + " these values are equal")
+// }
+
+// question 14
+
+// var a = Number(prompt ("Input your value"))
+// if (a < 0 ){
+//     document.write ("This number is Negative " + a )
+// }
+
+// else if (a > 0){
+//     document.write ("this number is positive " + a)
+// }
+// else {
+//     document.write ("This is Zero")
+// }
+
+
+// question 17
+
+// var vowel = prompt (" input your character ")
+// if (vowel === "a" || vowel === "e" || vowel === "i" || vowel === "o" || vowel === "u" ){
+//     document.write ("Yes this is vowel" )
+// }
+// else {
+//     document.write ("this is not a vowel")
+// }
+
+// question 18
+
+// var firstValue = 10
+// var secondValue = 8
+// var result = firstValue != secondValue
+// console.log(result)
+
+// question 19
+
+// var askMonth = +prompt("enter month number")
+// if (askMonth === 1){
+//     console.log("January")
+// }
+// else if (askMonth === 2){
+//     console.log("February")
+// }
+// else if (askMonth === 3){
+//     console.log("March")
+// }
+// else if (askMonth === 4){
+//     console.log("April")
+// }
+// else if (askMonth === 5){
+//     console.log("May")
+// }
+// else if (askMonth === 6){
+//     console.log("June")
+// }
+// else if (askMonth === 7){
+//     console.log("July")
+// }
+// else if (askMonth === 8){
+//     console.log("August")
+// }
+// else if (askMonth === 9){
+//     console.log("September")
+// }
+// else if (askMonth === 10){
+//     console.log("October")
+// }
+// else if (askMonth === 11){
+//     console.log("November")
+// }
+// else if (askMonth === 12){
+//     console.log("December")
+// }
+// else{
+//     console.log("invalid month")
+// }
+
+
+
+
+
+
+// var num1 = +prompt("first number ")
+// var operator = prompt("operator \n + \n - \n /  \n * \n % ")
+// var num2 = +prompt("second number")
+// function calculator(num1, num2, operator) {
+//     if (operator === "+") {
+//         document.write(num1 + num2)
+//     }
+//     else if (operator === "-") {
+//         document.write(num1 - num2)
+//     }
+//     else if (operator === "/") {
+//         document.write(num1 / num2)
+//     }
+//     else if (operator === "*") {
+//         document.write(num1 * num2)
+//     }
+//     else if (operator === "%") {
+//         document.write(num1 % num2)
+//     }
+//     else {
+//         document.write("syntex error")
+//     }
+// }
+// calculator(num1, num2, operator)
+
+
